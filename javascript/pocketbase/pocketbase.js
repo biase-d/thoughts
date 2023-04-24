d2996e458c530c192aa33db03599d18b162f3af3
itsTashi <tashingachikwanha@gmail.com>
2023-04-24 14:56:54 +0200

    fixing server address

--- javascript/pocketbase/pocketbase.js
+++ javascript/pocketbase/pocketbase.js
@@ -1,10 +1,5 @@
 import PocketBase from "./pocketbase-js-sdk/pocketbase.es.mjs";
 export { host }
 
-/* for production */
-const host = new PocketBase('https://petite-match.pockethost.io');
-
-
-/* for development
-const host = new PocketBase( 'http://127.0.0.1:8090' )
- */
+const url = 'https://petite-match.pockethost.io'
+const host = new PocketBase(url);
